8f95f7c443439a10e32e69671bc1353a38698aa5
Mohammad Fahad <mail.maohammadfahad@gmail.com>
2020-12-03 05:12:49 +0600

    search added

--- src/Component/HomePage/Header.js
+++ src/Component/HomePage/Header.js
@@ -16,7 +16,7 @@ const Header = ({ search, setSearch, searchBtn, setSearchBtn }) => {
                     <div className='container pt-5'>
                         <h1 className="text-white pt-5 text-center" ><strong>FIND YOUR HOUSE RENT</strong></h1>
                         <div className="d-flex justify-content-center">
-                            <input id="input" className="form-control p-3 mr-3" style={{ width: '600px' }} onBlur={(e) => setSearch(e.target.value.toLowerCase())} type="text" />
+                            <input id="input" className="form-control p-3 mr-3" style={{ width: '600px' }} onChange={(e) => setSearch(e.target.value.toLowerCase())} type="text" />
                             <button className="btn btn-success pr-3 pl-3" onClick={handleClick}>Find Now</button>
                         </div>
                     </div>
